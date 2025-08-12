afd69a8b8e4f5fa9435c84d5c0f2379ff9e49590
Rupesh <rupeshgiri30@gmail.com>
2025-08-11 23:00:55 -0400

    My second Java Project

---
+++ src/addition.java
@@ -0,0 +1,8 @@
+public class addition {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2=3;
+        int num3= num1 + num2;
+        System.out.println( " The sum of num1 and num2 is "+ num3);
+    }
+}
